b668a9ed293c3e8af78d89713c4fbc5d08ef6a8b
autumn-caer <muestra114@gmail.com>
2022-09-03 16:23:05 +0900

    add movie interface

--- front/frontend/src/ components/row.tsx
+++ front/frontend/src/ components/row.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "../axios";
 
 interface RowProps {
@@ -6,22 +6,39 @@ interface RowProps {
   fetchUrl: string;
 }
 
+interface Movie {
+  id: string;
+  poster_path: string;
+  name: string;
+}
+
+const baseUrl: string = "https://image.tmdb.org/t/p/original";
 const Row: React.FC<RowProps> = ({ title, fetchUrl }) => {
-  // const [movies, setMovies] = useState<Array<string>>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
     const fetchData = async (): Promise<any> => {
       const request = await axios.get(fetchUrl);
-      console.log(request);
+      setMovies(request.data.results);
       return request;
     };
 
-    const s = fetchData();
-    console.log(s);
+    fetchData().catch((err) => console.log(err));
   }, []);
   return (
     <div>
       <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie: Movie) => {
+          return (
+            <img
+              key={movie.id}
+              src={`${baseUrl}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
