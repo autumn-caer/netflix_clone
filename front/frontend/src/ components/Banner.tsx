9c45c8a7b133579f327d4a87abaffda986c62977
autumn-caer <muestra114@gmail.com>
2022-09-04 09:43:36 +0900

    add Banner

---
+++ front/frontend/src/ components/Banner.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import "./Banner.css";
+interface BannerProps {
+  fetchUrl: string;
+}
+
+interface Movie {
+  id: string;
+  poster_path: string;
+  name: string;
+  backdrop_path: string;
+  original_name: string;
+  title: string;
+  overview: string;
+}
+
+const baseUrl: string = "https://image.tmdb.org/t/p/original";
+const Banner: React.FC<BannerProps> = ({ fetchUrl }) => {
+  const [movie, setMovie] = useState<Movie>();
+
+  useEffect(() => {
+    const fetchData = async (): Promise<any> => {
+      const request = await axios.get(fetchUrl);
+
+      const targetMovie =
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ];
+      console.log(targetMovie);
+      setMovie(targetMovie);
+    };
+
+    fetchData().catch((err) => console.log(err));
+  }, []);
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("${baseUrl}${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title ?? movie?.name ?? movie?.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+
+        <h1 className="banner__description">{movie?.overview}</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Banner;
--- front/frontend/src/App.tsx
+++ front/frontend/src/App.tsx
@@ -1,30 +1,18 @@
 import React from "react";
 import "./App.css";
 import Row from "./ components/row";
+import Banner from "./ components/Banner";
 import requests from "./requests";
 function App(): JSX.Element {
   return (
     <div className="App">
       <h1>Hi Cleaver Programmer</h1>
+      <Banner fetchUrl={requests.fetchNetflixOriginals}></Banner>
       <Row
         title="Trendings"
         fetchUrl={requests.fetchTrending}
         isLarge={false}
       ></Row>
-      <Row
-        title="NetflixOriginals"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLarge={true}
-      ></Row>
-      <Row
-        title="TopRated"
-        fetchUrl={requests.fetchTopRated}
-        isLarge={true}
-      ></Row>
-      {/* <Row title="ActionMovies" fetchUrl={requests.fetchActionMovies}></Row>
-      <Row title="ComedyMovies" fetchUrl={requests.fetchComedyMovies}></Row>
-      <Row title="HorrorMovies" fetchUrl={requests.fetchHorrorMovies}></Row>
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row> */}
     </div>
   );
 }
